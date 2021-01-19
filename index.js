5f6da4d72f27b8d4790c85cc5d31ea766822331e
wallet77 <wallet77@gmail.com>
2021-01-19 01:14:37 +0100

    Add async utils

--- index.js
+++ index.js
@@ -1,5 +1,36 @@
 const axios = require('axios')
 const child_process = require('child_process')
+const path = require('path')
+
+const cloneRepo = (repository) => {
+    return new Promise((resolve, reject) => {
+        child_process.exec(`git clone ${repository}`, {
+            cwd: path.join(process.cwd(), 'repos')
+        }, (err, stdout) => {
+            const msg = `Clone ${repository}`
+            if (err) {
+                console.log(`${msg} => error`)
+                return reject(err)
+            }
+            console.log(`${msg} => done`)
+            return resolve(stdout)
+        })
+    })
+}
+
+const runQualscan = (repository) => {
+    return new Promise((resolve, reject) => {
+        child_process.exec(`git clone ${repository}`, (err, stdout) => {
+            const msg = `Clone ${repository}`
+            if (err) {
+                console.log(`${msg} => error`)
+                return reject(err)
+            }
+            console.log(`${msg} => done`)
+            return resolve(stdout)
+        })
+    })
+}
 
 const run = async () => {
     const res = await axios.get('https://gist.githubusercontent.com/anvaka/8e8fa57c7ee1350e3491/raw/b6f3ebeb34c53775eea00b489a0cea2edd9ee49c/01.most-dependent-upon.md')
@@ -7,33 +38,31 @@ const run = async () => {
     const allPackages = {}
 
     for (let i = 0; i < 10; i++) {
-        const package = packages[i]
+        const npmPackage = packages[i]
 
-        const name = package.substring(
-            package.lastIndexOf("[") + 1, 
-            package.lastIndexOf("]")
+        const name = npmPackage.substring(
+            npmPackage.lastIndexOf('[') + 1,
+            npmPackage.lastIndexOf(']')
         )
 
         if (name !== '') {
-            const url = package.substring(
-                package.lastIndexOf("(") + 1, 
-                package.lastIndexOf(")")
+            const url = npmPackage.substring(
+                npmPackage.lastIndexOf('(') + 1,
+                npmPackage.lastIndexOf(')')
             )
 
-            const rank = package.substring(
-                0, 
-                package.indexOf(".")
+            const rank = npmPackage.substring(
+                0,
+                npmPackage.indexOf('.')
             )
 
-            allPackages[name] = {
+            const currentPackage = {
                 name,
                 url,
                 rank
             }
 
             try {
-                // https://registry.npmjs.org/
-                // https://api.npms.io/v2/package/
                 const res = await axios.get(`https://registry.npmjs.org/${name}`)
                 let repository = res.data.repository
                 if (typeof res.data.repository === 'object') {
@@ -43,23 +72,24 @@ const run = async () => {
                 repository = repository.replace(/git\+/i, '')
                 repository = repository.replace(/\.git/i, '')
 
-                child_process.exec(`git clone ${repository}`, (err) => {
-                    console.log(`Cloning ${repository}`)
-                    if (err) {
-                        console.log(err)
-                        return console.log(' => error')
-                    }
-                    return console.log(' => success')
-                })
-                
+                const npmsRes = await axios.get(`https://api.npms.io/v2/package/${name}`)
+
+                currentPackage.npms = {
+                    final: npmsRes.data.score.final,
+                    quality: npmsRes.data.score.detail.quality
+                }
+
+                await cloneRepo(repository)
+                // await runQualscan(repository)
             } catch (err) {
                 console.log(err)
             }
+
+            allPackages[name] = currentPackage
         }
     }
 
-    // console.log(allPackages)
+    console.log(allPackages)
 }
-
 
 run()
