4f6ecdc540dcd8554f347c3fcfc3f3d728bdf846
wallet77 <wallet77@gmail.com>
2021-02-04 15:29:26 +0100

    Switch store to save report in a file.

--- src/store.js
+++ src/store.js
@@ -1,39 +1,13 @@
-const MongoClient = require('mongodb').MongoClient
 const logger = require('pino')()
-
-// Connection URL
-const url = process.env.MONGO_URL || 'mongodb://localhost:27017'
-
-// Database Name
-const dbName = 'npm-packages-quality-analysis'
-
-let client
-let reportsCollection
-
-const insertReports = async (report) => {
-    // Insert some reports
-    const result = await reportsCollection.insertMany([report])
-    return result
-}
-
-const connection = async () => {
-    client = await MongoClient.connect(url)
-    const db = client.db(dbName)
-    reportsCollection = db.collection('reports')
-}
+const fs = require('fs')
+const path = require('path')
 
 module.exports = {
     save: async (report) => {
-        let res
         try {
-            await connection()
-            res = await insertReports(report)
-            client.close()
+            await fs.promises.writeFile(path.join('./', 'report.json'), JSON.stringify(report), 'utf8')
         } catch (err) {
             logger.error(err)
         }
-
-        client.close()
-        return res
     }
 }
